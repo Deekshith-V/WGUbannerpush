c725eeffb079c587ae4797ff2d842bc7592aa02a
Deekshith-V <77414612+Deekshith-V@users.noreply.github.com>
2021-02-24 18:22:45 -0700

    changed Query in JPA

--- WguBannerPush/src/main/java/com/wgu/banner/jpa/StudentAdmissionDecisionRepository.java
+++ WguBannerPush/src/main/java/com/wgu/banner/jpa/StudentAdmissionDecisionRepository.java
@@ -1,22 +1,13 @@
 package com.wgu.banner.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.wgu.banner.entity.StudentAdmissionDecisionEntity;
 
 @Repository  // SARAPPD table
 public interface StudentAdmissionDecisionRepository extends JpaRepository<StudentAdmissionDecisionEntity, Integer>{
-	
-//	@Query(value = "select max(sarappd_appl_no)+1 from saturn.sarappd where sarappd_pidm = ?1 ", nativeQuery = true)
-//	public Integer getMaxApplNo(Integer studentAdmissionDecisionPIDM); // Using Query above to get max ApplNo for the PIDM and increment it for new record
-	
-//	public StudentAdmissionDecisionEntity findTopByStudentAdmissionDecisionPIDMOrderByStudentAdmissionDecisionApplNoDesc(Integer studentAdmissionDecisionPIDM);
-	
-//	@Query(value = "select max(sarappd_seq_no)+1 from saturn.sarappd where sarappd_pidm = ?1 and sarappd_term_code_entry = ?2 ", nativeQuery = true)
-//	public Integer getMaxSeqNo(Integer studentAdmissionDecisionPIDM, String studentAdmissionDecisionTermCode); // Using Query above to get max SeqNo for the PIDM and Term, and increment it for the new record
-	
+		
 	public StudentAdmissionDecisionEntity findTopByStudentAdmissionDecisionPIDMAndStudentAdmissionDecisionTermCodeOrderByStudentAdmissionDecisionSeqNoDesc(Integer studentAdmissionDecisionPIDM, String studentAdmissionDecisionTermCode);
 	
 }
